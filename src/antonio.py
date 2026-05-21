3728c73b3657bbb2bafd1630ba447db669802707
Antonio Junior <antoniorsjunior7405@gmail.com>
2026-05-21 19:23:27 -0300

    mudei algumas palavrs como crush e alguns prints

--- src/antonio.py
+++ src/antonio.py
@@ -1,36 +1,25 @@
-# Responsável pelo estado e progressão do relacionamento
+
 
 def atualizar_relacao(estado):
-    """
-    Verifica se o relacionamento subiu de nível
-    com base nos valores atuais de interesse e confiança.
-    Os estágios são: desconhecidos → amigos → crush → namoro
-    """
 
     relacao_atual = estado["relacao"]
 
     if relacao_atual == "desconhecidos" and estado["interesse"] >= 60:
         estado["relacao"] = "amigos"
-        print("\n💛 Vocês viraram amigos! O relacionamento evoluiu.")
+        print("\n💛 Você ainda é um beta!")
 
     elif relacao_atual == "amigos" and estado["interesse"] >= 75 and estado["confianca"] >= 50:
-        estado["relacao"] = "crush"
-        print("\n🧡 Ela te vê como um crush agora! As coisas estão esquentando.")
+        estado["relacao"] = "Pretendente"
+        print("\n🧡 Ela te vê como um pretendente agora! As coisas estão esquentando.")
 
-    elif relacao_atual == "crush" and estado["interesse"] >= 90 and estado["confianca"] >= 75:
+    elif relacao_atual == "Pretedente" and estado["interesse"] >= 90 and estado["confianca"] >= 75:
         estado["relacao"] = "namoro"
         print("\n❤️  Ela aceitou namorar! Você conseguiu!")
 
     return estado
 
 def verificar_limites(estado):
-    """
-    Garante que nenhuma variável passe dos limites permitidos.
-    Interesse e confiança ficam entre 0 e 100.
-    Humor também fica entre 0 e 100.
-    Isso evita que os números fiquem impossíveis (tipo interesse = 150).
-    """
-
+   
     if estado["interesse"] > 100:
         estado["interesse"] = 100
 
@@ -53,11 +42,7 @@ def verificar_limites(estado):
 
 
 def checar_condicao_encontro(estado):
-    """
-    Diz se as condições para marcar um encontro estão boas.
-    Útil para dar uma dica ao jogador antes de ele escolher.
-    Retorna True se for um bom momento, False se não for.
-    """
+
 
     confianca_ok = estado["confianca"] >= 50
     humor_ok = estado["humor"] > 40
